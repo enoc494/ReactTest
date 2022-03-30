3a6eb36b328057240ffe468760f568953a747aeb
enoc494 <angelavelarb.94@gmail.com>
2022-03-29 18:59:28 -0600

    intento de fetch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
+import {useEffect, useState} from "react";
 import Encabezado from "./componentes/Encabezado";
-import Fecha from "./componentes/Fecha";
-import Titulo from "./componentes/Titulo";
+import FechaTituloRow from "./componentes/FechaTituloRow"
 import Contenido from "./componentes/Contenido";
 import BotonesRow from "./componentes/BotonesRow";
 
+
+
+//Este componente debe considerarse una "tarjeta de la app"
+//Aquí se consumirá la api y se enviará la info a las tarjetas
+
+
 function App() {
+  //Definiciones
+  const [tender, setTender] = useState([]);
+ 
+  //Cambiar hook para la actualización
+   useEffect( ()=>{
+    fetch("https://tenders.guru/api/es/tenders",  { mode: 'no-cors'})
+    .then( r => r.json() )
+    .then(data =>{
+      console.log(data);
+    })
+  },[])
   return (
-    <div className="App">
+    <div>
     <Encabezado/> 
-    <Fecha/>
-         
+    <FechaTituloRow/>
+    <Contenido/>
+    <BotonesRow/>
       
     </div>
   );
---
+++ src/componentes/FechaTituloRow.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Fecha from './Fecha'
+import Titulo from './Titulo'
+
+export default function FechaTituloRow() {
+  return (
+    <div>
+      <Fecha/>
+      <Titulo/>  
+    </div>
+  )
+}
